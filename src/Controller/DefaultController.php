87192d076e699e6fd842f80485dcc678e34f0354
sekarpov <sergo8ck@gmail.com>
2021-02-02 18:22:32 +0300

    Миграции и Сервис контейнеры

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Services\GiftsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    public function __construct(GiftsService $gifts)
+    {
+        $gifts->gifts = ['a', 'b', 'c'];
+    }
+
     /**
      * @Route("/default", name="default")
      */
-    public function index(): Response
+    public function index(GiftsService $gifts): Response
     {
+//        $users = ['Adam', 'Robert', 'John', 'Susan'];
+        $entityManager = $this->getDoctrine()->getManager();
+
+
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'users' => $users,
+            'random_gift' => $gifts->gifts,
         ]);
     }
 }
---
+++ src/Services/GiftsService.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Services;
+
+
+class GiftsService
+{
+    public $gifts = ['flowers', 'car', 'piano', 'money'];
+
+    public function __construct()
+    {
+        shuffle($this->gifts);
+    }
+}
