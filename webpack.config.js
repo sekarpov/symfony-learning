af38125e2e9897f216ceec068cf2a3e97b0d06b1
sekarpov <sergo8ck@gmail.com>
2021-02-04 14:24:26 +0300

    twig - webpack и подключение jquery

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setPublicPath('/build')
+    .setOutputPath('public/build/')
+    .addEntry('js/custom', './build/js/custom.js')
+    .addStyleEntry('css/custom', './build/css/custom.css')
+    .disableSingleRuntimeChunk()
+;
+
+module.exports = Encore.getWebpackConfig();
