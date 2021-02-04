159d85cc621ba8be3ee8986f213df29253a6c70b
sekarpov <sergo8ck@gmail.com>
2021-02-04 12:26:44 +0300

    некоторые возможности twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,9 +7,7 @@ use App\Services\GiftsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 
 class DefaultController extends AbstractController
@@ -20,134 +18,23 @@ class DefaultController extends AbstractController
     }
 
     /**
-     * @Route("/default", name="default")
+     * @Route("/page", name="default")
      */
-    public function index(GiftsService $gifts, Request $request): Response
+    public function index(Request $request): Response
     {
-        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+//        $entityManager = $this->getDoctrine()->getManager();
+//        $user = $entityManager->getRepository(User::class)->find(1);
 
-        if(!$users){
-            throw $this->createNotFoundException("The users do not exist");
-        }
+//        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+//        if(!$users){
+//            throw $this->createNotFoundException("The users do not exist");
+//        }
+
+
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
-            'users' => $users,
-            'random_gift' => $gifts->gifts
         ]);
-    }
-
-    /**
-     * @Route("/blog/{page?}", name="blog_list", requirements={"page"="\d+"})
-     */
-    public function index2()
-    {
-        return new Response('Optional parameters in url and requirements for parameters');
-    }
-
-    /**
-     * Примеры маршрута (
-     *  http://localhost:7777/articles/en/2019/dell/rtv
-     *  http://localhost:7777/articles/en/2019/dell
-     * )
-     *
-     * @Route(
-     *     "/articles/{_locale}/{year}/{slug}/{category}",
-     *     defaults={"category": "computers"},
-     *     requirements={
-     *         "_locale": "en|fr",
-     *         "category": "computers|rtv",
-     *         "year": "\d+"
-     *     }
-     * )
-     */
-    public function index3()
-    {
-        return new Response('An advanced route example');
-    }
-
-    /**
-     * Примеры маршрута (
-     *  http://localhost:7777/over-ons
-     *  http://localhost:7777/about-us
-     * )
-     *
-     * @Route({
-     *     "nl": "/over-ons",
-     *     "en": "/about-us",
-     * }, name="about_us")
-     */
-    public function index4()
-    {
-        return new Response('Translated routes');
-    }
-
-    /**
-     * Генерация URL
-     *
-     * @Route("/generate-url/{param?}", name="generate_url")
-     */
-    public function generate_url()
-    {
-        dd($this->generateUrl(
-            'generate_url',
-            [
-                'param' => 10
-            ],
-            UrlGeneratorInterface::ABSOLUTE_URL
-        ));
-    }
-
-    /**
-     * Скачивание файла
-     *
-     * @Route("/download")
-     */
-    public function download()
-    {
-        $path = $this->getParameter('download_directory');
-        return $this->file($path.'file.pdf');
-    }
-
-    /**
-     * Редирект
-     *
-     * @Route("/redirect-test")
-     */
-    public function redirectTest()
-    {
-        return $this->redirectToRoute('route_to_redirect', ['param' => 10]);
-    }
-
-    /**
-     * @Route("/url-to-redirect/{param?}", name="route_to_redirect")
-     */
-    public function methodToRedirect()
-    {
-        dd('Test redirection');
-    }
-
-    /**
-     * Ретрансляция на другие контроллеры и методы
-     *
-     * @Route("/forward-to-controller")
-     */
-    public function forwardToController()
-    {
-        $response = $this->forward(
-          'App\Controller\DefaultController::methodToForwardTo',
-            ['param'=> '1']
-        );
-
-        return $response;
-    }
-
-    /**
-     * @Route("/url-to-forward-to/{param?}", name="route_to_forward_to")
-     */
-    public function methodToForwardTo($param)
-    {
-        dd('Test controller forward - ' . $param);
     }
 
 
