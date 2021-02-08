8f1dc9299df2301472c359355d55643ef32c1395
skarpov <sergo8ck@gmail.com>
2021-02-08 12:45:34 +0300

    пакет sensio/framework-extra-bundle - параметры доктрины

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Services\GiftsService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,32 +18,14 @@ class DefaultController extends AbstractController
     }
 
     /**
-     * @Route("/home", name="default", name="home")
+     * @Route("/home/{id}", name="default", name="home")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, User $user): Response
     {
-        $entityManager = $this->getDoctrine()->getManager();
-        #1
-//        $query = $entityManager->createQuery(
-//                'SELECT u
-//            FROM App\Entity\User u
-//            WHERE u.id > :id
-//            ORDER BY u.id ASC'
-//        )->setParameter('id', 2);
-//        $users = $query->getResult();
-//        dd($users);
+//        $entityManager = $this->getDoctrine()->getManager();
 
-        #2 (почему-то не работает)
-//        $conn = $entityManager->getConnection();
-//        $sql = '
-//            SELECT * FROM user u
-//            WHERE u.id > :id
-//            ORDER BY u.id ASC
-//            ';
-//        $stmt = $conn->prepare($sql);
-//        $stmt->execute(['id' => 2]);
-//        $users = $stmt->fetchAll();
-//        dd($users);
+        dump($user);
+
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
