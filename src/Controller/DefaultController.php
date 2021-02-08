67ec0c12d5a0c08733b163850b50a32c879430ad
skarpov <sergo8ck@gmail.com>
2021-02-08 11:58:01 +0300

    doctrine чистые запросы

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -22,6 +22,28 @@ class DefaultController extends AbstractController
      */
     public function index(Request $request): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        #1
+//        $query = $entityManager->createQuery(
+//                'SELECT u
+//            FROM App\Entity\User u
+//            WHERE u.id > :id
+//            ORDER BY u.id ASC'
+//        )->setParameter('id', 2);
+//        $users = $query->getResult();
+//        dd($users);
+
+        #2 (почему-то не работает)
+//        $conn = $entityManager->getConnection();
+//        $sql = '
+//            SELECT * FROM user u
+//            WHERE u.id > :id
+//            ORDER BY u.id ASC
+//            ';
+//        $stmt = $conn->prepare($sql);
+//        $stmt->execute(['id' => 2]);
+//        $users = $stmt->fetchAll();
+//        dd($users);
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
