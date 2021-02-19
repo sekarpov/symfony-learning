375925b6ebdf19d7df8174a7d8c77cef919cc6a4
skarpov <sergo8ck@gmail.com>
2021-02-19 17:32:33 +0300

    Кеш - теги

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,6 +14,7 @@ use App\Services\MyService;
 use App\Services\ServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\TagAwareAdapter;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -34,18 +35,55 @@ class DefaultController extends AbstractController
     {
         $entityManager = $this->getDoctrine()->getManager();
 
-        $cache = new FilesystemAdapter();
-        $posts = $cache->getItem('database.get_posts');
-        if (!$posts->isHit()){
-            $posts_from_db = ['post1', 'post2', 'post3'];
-            dump('connected with database ...');
-            $posts->set(serialize($posts_from_db));
-            $posts->expiresAfter(5);
-            $cache->save($posts);
+        $cache = new TagAwareAdapter(
+                new FilesystemAdapter()
+        );
+
+        $acer = $cache->getItem('acer');
+        $dell = $cache->getItem('dell');
+        $ibm = $cache->getItem('ibm');
+        $apple = $cache->getItem('apple');
+
+        if(!$acer->isHit()){
+            $acer_from_db = 'acer laptop';
+            $acer->set($acer_from_db);
+            $acer->tag(['computers', 'laptops', 'acer']);
+            $cache->save($acer);
+            dump('acer laptor from database ... ');
         }
-//        $cache->deleteItem('database.get_posts');
-//        $cache->clear();
-        dump(unserialize($posts->get()));
+        if(!$dell->isHit()){
+            $dell_from_db = 'dell laptop';
+            $dell->set($dell_from_db);
+            $dell->tag(['computers', 'laptops', 'dell']);
+            $cache->save($dell);
+            dump('dell laptor from database ... ');
+        }
+
+        if(!$ibm->isHit()){
+            $ibm_from_db = 'ibm laptop';
+            $ibm->set($ibm_from_db);
+            $ibm->tag(['computers', 'desktops', 'ibm']);
+            $cache->save($ibm);
+            dump('ibm desktop from database ... ');
+        }
+        if(!$apple->isHit()){
+            $apple_from_db = 'apple laptop';
+            $apple->set($apple_from_db);
+            $apple->tag(['computers', 'desktops', 'apple']);
+            $cache->save($apple);
+            dump('apple desktop from database ... ');
+        }
+
+//        $cache->invalidateTags(['ibm']);
+//        $cache->invalidateTags(['desktops']);
+        $cache->invalidateTags(['laptops']);
+//        $cache->invalidateTags(['computers']);
+
+
+        dump($acer->get());
+        dump($dell->get());
+        dump($ibm->get());
+        dump($apple->get());
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
