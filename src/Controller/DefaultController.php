475dac23182d63d7eb55d36b62b4a3fd7e342415
skarpov <sergo8ck@gmail.com>
2021-02-19 16:57:56 +0300

    Кеш - базовое использование

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -36,6 +36,15 @@ class DefaultController extends AbstractController
 
         $cache = new FilesystemAdapter();
         $posts = $cache->getItem('database.get_posts');
+        if (!$posts->isHit()){
+            $posts_from_db = ['post1', 'post2', 'post3'];
+            dump('connected with database ...');
+            $posts->set(serialize($posts_from_db));
+            $posts->expiresAfter(5);
+            $cache->save($posts);
+        }
+//        $cache->deleteItem('database.get_posts');
+//        $cache->clear();
         dump(unserialize($posts->get()));
 
         return $this->render('default/index.html.twig', [
