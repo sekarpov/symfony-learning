2817f0cbbfaf77e7e86b4986cf2ec22a4c7f3808
skarpov <sergo8ck@gmail.com>
2021-02-20 11:38:08 +0300

    Использование profiler toll и установка пакета debug-bundle

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -35,55 +35,7 @@ class DefaultController extends AbstractController
     {
         $entityManager = $this->getDoctrine()->getManager();
 
-        $cache = new TagAwareAdapter(
-                new FilesystemAdapter()
-        );
-
-        $acer = $cache->getItem('acer');
-        $dell = $cache->getItem('dell');
-        $ibm = $cache->getItem('ibm');
-        $apple = $cache->getItem('apple');
-
-        if(!$acer->isHit()){
-            $acer_from_db = 'acer laptop';
-            $acer->set($acer_from_db);
-            $acer->tag(['computers', 'laptops', 'acer']);
-            $cache->save($acer);
-            dump('acer laptor from database ... ');
-        }
-        if(!$dell->isHit()){
-            $dell_from_db = 'dell laptop';
-            $dell->set($dell_from_db);
-            $dell->tag(['computers', 'laptops', 'dell']);
-            $cache->save($dell);
-            dump('dell laptor from database ... ');
-        }
-
-        if(!$ibm->isHit()){
-            $ibm_from_db = 'ibm laptop';
-            $ibm->set($ibm_from_db);
-            $ibm->tag(['computers', 'desktops', 'ibm']);
-            $cache->save($ibm);
-            dump('ibm desktop from database ... ');
-        }
-        if(!$apple->isHit()){
-            $apple_from_db = 'apple laptop';
-            $apple->set($apple_from_db);
-            $apple->tag(['computers', 'desktops', 'apple']);
-            $cache->save($apple);
-            dump('apple desktop from database ... ');
-        }
-
-//        $cache->invalidateTags(['ibm']);
-//        $cache->invalidateTags(['desktops']);
-        $cache->invalidateTags(['laptops']);
-//        $cache->invalidateTags(['computers']);
-
-
-        dump($acer->get());
-        dump($dell->get());
-        dump($ibm->get());
-        dump($apple->get());
+        dump($request);
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
